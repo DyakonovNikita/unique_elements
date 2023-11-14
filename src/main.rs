20b3f3cbbf20448b96534d476b175ab1bd8c6e14
Nikita Dyakonov <roboromapie@gmail.com>
2023-11-14 03:23:14 +0300

    use io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::str::FromStr;
+
 fn unique_count(v: &mut Vec<i32>) -> usize {
     v.sort();
     v.dedup();
@@ -5,7 +8,17 @@ fn unique_count(v: &mut Vec<i32>) -> usize {
 }
 
 fn main() {
-    let mut numbers = vec![1, 2, 3, 4, 5, 1, 2, 3, 4];
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Не удалось прочитать строку");
+
+    let mut numbers: Vec<i32> = input
+        .trim()
+        .split(", ")
+        .map(|s| i32::from_str(s).expect("Не удалось преобразовать строку в число"))
+        .collect();
+    // let mut numbers = vec![1, 2, 3, 4, 5, 1, 2, 3, 4];
     let res = unique_count(&mut numbers);
     println!("Количество уникальных элементов: {}", res)
 }
